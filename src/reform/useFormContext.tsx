c19b8183ef706cbcb714416baab5508bb0ecca82
Franck Wolff <frawolff@gmail.com>
2025-09-03 09:52:51 +0200

    Make useFormContext generic

--- src/reform/useFormContext.tsx
+++ src/reform/useFormContext.tsx
@@ -3,6 +3,6 @@ import { FormManager } from "./FormManager"
 
 export const FormContext = React.createContext<FormManager<unknown> | null>(null)
 
-export function useFormContext() {
-    return React.useContext(FormContext)!
+export function useFormContext<T = unknown>() {
+    return React.useContext(FormContext)! as FormManager<T>
 }

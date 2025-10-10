ef40a7adc34b835393bdf4692201ae50155f2a9e
Franck Wolff <frawolff@gmail.com>
2025-10-10 16:45:04 +0200

    Add BaseTextAreaField

---
+++ src/reform/components/BaseTextAreaField.tsx
@@ -0,0 +1,77 @@
+import React, { DOMAttributes, TextareaHTMLAttributes, useRef } from "react"
+import { useFormField } from "../useFormField"
+import { ReformEvents } from "./InputHTMLProps"
+
+export type BaseTextAreaFieldHTMLAttributes = (
+    Omit<TextareaHTMLAttributes<HTMLTextAreaElement>,
+        // HTMLAttributes
+        'name' |
+
+        'value' |
+
+        'defaultValue' |
+        'defaultChecked' |
+        'suppressContentEditableWarning' |
+        'suppressHydrationWarning' |
+
+        'contentEditable' |
+        'contextMenu' |
+        'hidden' |
+        'is' |
+
+        // TextareaHTMLAttributes
+        'autoComplete' |
+        'dirName' |
+        'form' |        
+
+        keyof DOMAttributes<HTMLTextAreaElement>
+    > &
+    {
+        name: string
+    }
+)
+
+export type BaseTextAreaFieldProps = BaseTextAreaFieldHTMLAttributes & ReformEvents<string> & {
+    render: () => void
+}
+
+export function BaseTextAreaField(props: BaseTextAreaFieldProps) {
+
+    const { render, onChange, onBlur, ...textAreaProps } = props
+    const { value: fieldValue, form } = useFormField<string | null, number>(props.name)
+
+    const textAreaRef = useRef<HTMLTextAreaElement>(null)
+
+    const internalOnChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const value = event.currentTarget.value || null
+        if (value !== fieldValue) {
+            form.setValue(props.name, value)
+            form.validateAt(props.name) && render()
+            onChange?.(value, form)
+        }
+    }
+
+    const internalOnBlur = (event: React.FocusEvent<HTMLTextAreaElement>) => {
+        const value = event.currentTarget.value || null
+        form.setValue(props.name, value, true)
+        onBlur?.(value, form)
+    }
+
+    // If this is the first render or if this textarea isn't currently edited
+    if (textAreaRef.current == null || textAreaRef.current !== document.activeElement) {
+        const value = fieldValue ?? ""
+        if (textAreaRef.current != null)
+            textAreaRef.current.value = value
+        else
+            (textAreaProps as TextareaHTMLAttributes<HTMLTextAreaElement>).defaultValue = value
+    }
+
+    return (
+        <textarea
+            { ...textAreaProps }
+            ref={ textAreaRef }
+            onChange={ internalOnChange }
+            onBlur={ internalOnBlur }
+        />
+    )
+}

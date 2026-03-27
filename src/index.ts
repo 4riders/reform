cbcefc221e3cac6d6fa2332ff732143ececdb1b6
Franck Wolff <frawolff@gmail.com>
2026-03-27 14:14:43 +0100

    More JSDoc comments

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,47 @@
 /**
  * @packageDocumentation
  * 
+ * @categoryDescription Property Decorators
+ * Property decorators are a powerful feature in Javascript that allow you to annotate and modify the behavior of class properties.
+ * In the context of form management, property decorators can be used to define metadata for form fields, such as validation rules,
+ * input types, or custom behavior. By applying decorators to your form model classes, you can easily specify how each field should
+ * be handled without having to write additional boilerplate code. This makes it easier to maintain and extend your form models as your
+ * application grows.
+ * 
+ * @categoryDescription Class Decorators
+ * Class decorators are a type of decorator in JavaScript that can be applied to classes to modify their behavior or add metadata.
+ * In the context of form management, class decorators can be used to define metadata for form models, such as validation rules.
+ * 
+ * @categoryDescription Shared Constraints
+ * Shared constraints are common validation rules that can be applied to form fields or entire form models. They define the conditions
+ * that the field values must satisfy in order to be considered valid. Constraints can be simple, such as checking if a value is required
+ * or if it falls within a certain range, or they can be complex, involving custom logic that depends on multiple fields.
+ * 
  * @categoryDescription Observers
  * Observers allow you to react to changes in other fields by specifying a path to observe and a callback function that
  * receives context about the change. The observer paths support a flexible syntax for targeting specific fields, including
  * wildcards, absolute and relative paths.
  * 
- * @categoryDescription Shared Constraints
- * Constraints are validation rules that can be applied to form fields or entire form models. They define the conditions that the field
- * values must satisfy in order to be considered valid. Constraints can be simple, such as checking if a value is required or if it falls
- * within a certain range, or they can be complex, involving custom logic that depends on multiple fields. By applying constraints to your form
- * models, you can ensure that the data entered by users meets the necessary criteria before it is submitted or processed.
+ * @categoryDescription Form Management
+ * Form management involves handling the state and behavior of forms in a web application. This includes managing form data, validation,
+ * and user interactions. A form management library can provide tools and utilities to simplify these tasks, allowing developers to
+ * focus on building the user interface and business logic of their applications.
+ * 
+ * @categoryDescription Base Inputs Components
+ * Base input components are reusable building blocks for creating form fields in a web application. They provide a consistent interface and
+ * behavior for common input types, such as text fields, checkboxes, radio buttons, and select dropdowns.
+ * 
+ * @categoryDescription Validation Management
+ * Validation management involves defining and enforcing rules for validating form data. This can include simple checks, such as ensuring
+ * that a field is not empty, as well as more complex rules that involve multiple fields or custom logic.
+ * 
+ * @categoryDescription Localization
+ * Localization is the process of adapting a product or content to a specific locale or market. In the context of form management, localization
+ * involves providing translations for error messages.
+ * 
+ * @categoryDescription Utilities
+ * Utilities are helper functions or classes that provide common functionality that can be reused across different parts of an application. In the
+ * context of form management, utilities include functions for manipulating and comparing form data.
  */
 
 export * from "./reform/ArrayHelper"
